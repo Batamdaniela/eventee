1ff20bf6d5d23afce9a1700e0261b02ecb8997ba
Batamdaniela <mydanielabatam@gmail.com>
2024-10-24 16:11:48 +0100

    [Feat]: Configuration de Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        './templates/**/*.html',
+        './salles/templates/**/*.html',
+        './static/**/*.js',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
